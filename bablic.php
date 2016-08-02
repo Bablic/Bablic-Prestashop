8f138eccb6f39a7fc5cb23038f3ee8288f7c0504
Ishai Jaffe <receipts@bablic.com>
2016-08-02 11:19:51 +0300

    Update bablic.php

--- bablic.php
+++ bablic.php
@@ -1,28 +1,5 @@
 <?php
-/*
-	Module Name: Bablic
-	Module URI: https://www.bablic.com/
-	Description: Connects your Prestashop to every language instantly
-	Version: 0.1.0
-	Author: Ishai Jaffe
-	Author URI: https://www.bablic.com/
-	
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-	
-	This copyright notice  and licence should be retained in all modules based on this framework.
-	This does not affect your rights to assert copyright over your own original work.
-*/
 
 class Bablic extends Module
 {
