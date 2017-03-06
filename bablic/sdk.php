a511a112655f1c3562447f9518cc45c9c036039e
Ishai <ishai@bablic.com>
2017-03-06 09:33:53 +0200

    validated

--- bablic/sdk.php
+++ bablic/sdk.php
@@ -1,19 +1,29 @@
 <?php
-
 /**
- * Bablic Localization.
- *
- * LICENSE: This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * @category  localization
- *
- * @author    Ishai Jaffe <ishai@bablic.com>
- * @copyright Bablic 2017
- * @license   http://www.gnu.org/licenses/ GNU License
- */
+* 2007-2017 PrestaShop.
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author    PrestaShop SA <contact@prestashop.com>
+*  @copyright 2007-2015 PrestaShop SA
+*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
+
 class BablicSDK
 {
     public $site_id = '';
